f501a16b25098b503d49f5a2b4aa8738bbd7cbf4
Bill <stackeduary@gmail.com>
2022-12-02 00:54:18 +0200

    adding Day 1 solutions

--- src/Day1/Day1Part2.java
+++ src/Day1/Day1Part2.java
@@ -13,19 +13,18 @@ public class Day1Part2 {
         String strLine;
         Scanner sc = new Scanner(file);
         int elfCalories = 0;
-        ArrayList<Integer> topThreeElves = new ArrayList<Integer>();
+        ArrayList<Integer> allElves = new ArrayList<Integer>();
         while (sc.hasNextLine()) {
             try {
                 int calories = Integer.parseInt(sc.nextLine());
                 elfCalories += calories;
             } catch (Exception e) {
-                topThreeElves.add(elfCalories);
+                allElves.add(elfCalories);
                 elfCalories = 0;
             }
         }
-        topThreeElves.sort(Comparator.reverseOrder());
-        int sumTopThree = 0;
-        sumTopThree = topThreeElves.get(0) + topThreeElves.get(1) + topThreeElves.get(2);
+        allElves.sort(Comparator.reverseOrder());
+        int sumTopThree = allElves.get(0) + allElves.get(1) + allElves.get(2);
         System.out.println(sumTopThree);
     }
 }
